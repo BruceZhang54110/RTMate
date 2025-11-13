a7a804a49b040ab28d8406f659ea19da5966c770
BruceZhang54110 <zhw54110@163.com>
2025-11-13 23:08:50 +0800

    日志打印格式调整

--- rtmate-server/src/bootstrap.rs
+++ rtmate-server/src/bootstrap.rs
@@ -2,14 +2,26 @@ use tracing_subscriber::{EnvFilter, fmt, registry};
 use tracing_subscriber::prelude::*; // bring SubscriberExt into scope for .with()
 use std::sync::Arc;
 use crate::web_context::WebContext;
+use tracing_subscriber::fmt::time::LocalTime;
+use time::macros::format_description;
 
 pub fn init_tracing() {
+    let timer = LocalTime::new(format_description!("[year]-[month]-[day] [hour]:[minute]:[second].[subsecond digits:3]"));
+
     registry()
         .with(
             EnvFilter::try_from_default_env()
                 .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
         )
-        .with(fmt::layer())
+        .with(fmt::layer()
+            .with_thread_ids(true)
+            .with_thread_names(true)
+            .with_target(false)
+            .with_level(true)
+            .with_line_number(true)
+            .with_file(true)
+            .with_timer(timer)
+        )
         .init();
 }
 
